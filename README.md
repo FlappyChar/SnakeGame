2c08b2696a29967e0f5d07eb686b42fd35a2db68
FlappyChar <stall.1996@mail.ru>
2025-05-18 04:05:36 +0300

    Исходная фиксация

---
+++ README.md
@@ -0,0 +1,3 @@
+# SnakeGames
+
+Developed with Unreal Engine 5
---
+++ Source/SnakeGames/SnakeGames.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnakeGames.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnakeGames, "SnakeGames" );
